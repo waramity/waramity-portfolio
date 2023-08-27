7af2c22cf02b2953d9a84742cba8c6138ded1794
unknown <waramity.re@gmail.com>
2023-08-27 19:49:47 +0700

    add session pop in logout auth dating

--- app/features/auth/routes.py
+++ app/features/auth/routes.py
@@ -45,6 +45,7 @@ def before_request():
 @login_required
 def logout():
     logout_user()
+    session.pop('platform', None)
     return redirect(url_for('main.index'))
 
 def get_google_provider_cfg():
@@ -52,12 +53,9 @@ def get_google_provider_cfg():
 
 @auth.route("/google-auth")
 def google_auth():
-    # Find out what URL to hit for Google login
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-    # Use library to construct the request for Google login and provide
-    # scopes that let you retrieve user's profile from Google
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
         redirect_uri=request.base_url + "/callback",
@@ -67,15 +65,11 @@ def google_auth():
 
 @auth.route("/google-auth/callback")
 def google_auth_callback():
-    # Get authorization code Google sent back to you
     code = request.args.get("code")
 
-    # Find out what URL to hit to get tokens that allow you to ask for
-    # things on behalf of a user
     google_provider_cfg = get_google_provider_cfg()
     token_endpoint = google_provider_cfg["token_endpoint"]
 
-    # Prepare and send a request to get tokens! Yay tokens!
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
@@ -89,27 +83,17 @@ def google_auth_callback():
         auth=(app.config['GOOGLE_CLIENT_ID'], app.config['GOOGLE_CLIENT_SECRET']),
     )
 
-    # Parse the tokens!
     client.parse_request_body_response(json.dumps(token_response.json()))
 
-    # Now that you have tokens (yay) let's find and hit the URL
-    # from Google that gives you the user's profile information,
-    # including their Google profile image and email
     userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
     uri, headers, body = client.add_token(userinfo_endpoint)
     userinfo_response = requests.get(uri, headers=headers, data=body)
 
-    # You want to make sure their email is verified.
-    # The user authenticated with Google, authorized your
-    # app, and now you've verified their email through Google!
     if userinfo_response.json().get("email_verified"):
         social_auth_id = userinfo_response.json()["sub"]
     else:
         return "User email not available or not verified by Google.", 400
 
-
-    # Create a user in your db with the information provided
-    # by Google
     user_social = UserSocial.query.filter_by(social_auth_id=social_auth_id).first()
     if not user_social:
         unique_id = uuid.uuid4().hex
@@ -133,12 +117,10 @@ def google_auth_callback():
     else:
         user = User.query.filter_by(id=user_social.user_id).first()
 
-    # Begin user session by logging the user in
     login_user(user)
 
     session['platform'] = 'dating'
 
-    # Send user back to homepage
     return redirect(url_for('main.index'))
 
 def render_picture(data):
@@ -157,16 +139,10 @@ def create_profile():
         birth_month = form.birth_month.data
         birth_year = form.birth_year.data
         birthday_str = str(birth_month) + "/" + str(birth_day) + "/" + str(birth_year)
-        # birthday_datetime = datetime.datetime(year=int(birth_year), month=int(birth_month), day=int(birth_day))
         birthday_datetime = datetime.datetime.strptime(birthday_str, '%m/%d/%Y')
 
         gender_name = form.gender.data
         gender_id = Gender.query.filter_by(name=gender_name).first().id
-        #
-        # sexual_orientation_names = form.sexual_orientation.data
-        # sexual_orientations = []
-        # for name in sexual_orientation_names:
-        #     sexual_orientations.append(SexualOrientation.query.filter_by(name=name).first())
 
         showme_names = form.showme.data
         showmes = []
@@ -217,7 +193,6 @@ def create_profile():
         current_user.given_name = given_name
         current_user.birthday = birthday_datetime
         current_user.gender_id = gender_id
-        # current_user.sexual_orientations = sexual_orientations
         current_user.preferences = preferences
         current_user.passions = passions
         current_user.profile_images = profile_images
@@ -230,60 +205,6 @@ def create_profile():
     genders = Gender.query.all()
     passions = Passion.query.all()
     return render_template('auth/create-profile.html', title=_('สร้างโปรไฟล์ - Dootua'), genders=genders, passions=passions, form=form)
-    # return render_template('auth/create-profile.html', title=_('สร้างโปรไฟล์ - Dootua'), genders=genders, sexual_orientations=sexual_orientations, passions=passions, form=form)
-
-#
-# @auth.route("/create-profile", methods=['POST'])
-# @login_required
-# def create_profile_post():
-#     given_name = request.form.get('given_name')
-#
-#     birth_day = request.form.get('birth_day')
-#     birth_month = request.form.get('birth_month')
-#     birth_year = request.form.get('birth_year')
-#     birthday_datetime = datetime.datetime(year=int(birth_year), month=int(birth_month), day=int(birth_day))
-#
-#     gender_id = request.form.get('gender_id')
-#
-#     sexual_orientation_ids = request.form.getlist('sexual-orientation')
-#     sexual_orientations = []
-#     for id in sexual_orientation_ids:
-#         sexual_orientations.append(SexualOrientation.query.filter_by(id=id).first())
-#
-#     showme_gender_ids = request.form.getlist('showme')
-#     showme_genders = []
-#     for id in showme_gender_ids:
-#         showme_genders.append(Gender.query.filter_by(id=id).first())
-#
-#     passion_ids = request.form.getlist('passion')
-#     passions = []
-#     for id in passion_ids:
-#         passions.append(Passion.query.filter_by(id=id).first())
-#
-#     profile_image_files = request.files.getlist("profile_image")
-#     profile_images = []
-#
-#     for file in profile_image_files:
-#         if file.filename == "":
-#             continue
-#         data = file.read()
-#         render_file = render_picture(data)
-#
-#         profile_image = ProfileImage(name=file.filename, data=data, rendered_data=render_file)
-#         profile_images.append(profile_image)
-#         db.session.add(profile_image)
-#
-#     current_user.given_name = given_name
-#     current_user.birthday = birthday_datetime
-#     current_user.gender = gender_id
-#     current_user.sexual_orientations = sexual_orientations
-#     current_user.showme_genders = showme_genders
-#     current_user.passions = passions
-#     current_user.profile_images = profile_images
-#
-#     db.session.commit()
-#
-#     return redirect(url_for('auth.profile'))
 
 @auth.route('/profile')
 @login_required
