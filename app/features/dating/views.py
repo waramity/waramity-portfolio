1c8bcba794bda63eb8755bf93c436a9ff098d45c
unknown <waramity.re@gmail.com>
2023-08-30 17:20:43 +0700

    praise chatgpt

--- app/features/dating/views.py
+++ app/features/dating/views.py
@@ -8,6 +8,8 @@ from app.models import Matches, Message, User
 import uuid
 import json
 import datetime, time
+from sqlalchemy import func
+
 
 from app import db
 
@@ -34,7 +36,12 @@ def userConnected():
             user_id = match.sender_id
 
         recipient_user = User.query.filter(User.id == user_id).first()
-        last_message = Message.query.filter(Message.match_id == match.id).order_by(Message.created_date.desc()).first()
+        last_message = Message.query.filter(Message.match_id == match.id).order_by(Message.created_date.desc(), func.random()).first()
+        messages = Message.query.filter(Message.match_id == match.id).order_by(Message.created_date.desc()).all()
+        for msg in messages:
+            print(msg.message)
+            print(msg.created_date)
+        print(last_message.message)
         message = None
         if last_message:
             message = {"message": last_message.message, "datetime": json.dumps(last_message.created_date, default=stringifyDateTime)}
@@ -72,6 +79,7 @@ def message(form):
     if not user:
         flash("No such account exists.")
         return redirect(url_for("main.app"))
+
     new_message = Message(id=random_uuid(Message), match_id=match_id, sender_id=current_user.id, message=message)
     db.session.add(new_message)
     db.session.commit()
