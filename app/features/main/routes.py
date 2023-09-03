3755706c80406c6d26884ae22b7c7e79fa6ec9ff
unknown <waramity.re@gmail.com>
2023-09-04 03:41:43 +0700

    add uwsgi.ini

--- app/features/main/routes.py
+++ app/features/main/routes.py
@@ -32,19 +32,19 @@ def load_json(filename):
 @main.route('/')
 def index():
     session['platform'] = 'none'
-    skill_data = load_json('data/main/skill.json')
+    skill_data = load_json('data/skill.json')
     system_architecture_icons = ['python.png', 'javascript.png', 'html.png', 'css.png', 'jquery.png', 'scss.png', 'type-script.png', 'babel.png', 'flask.png', 'sqlalchemy.png', 'react.png', 'socket-io.png', 'digital-ocean.png', 'mongo.jpg', 'tailwind.png', 'oauth.png', 'postgres.png', 'bootstrap.webp']
     return render_template('main/index.html', title=_('waramity portfolio'), skill_data=skill_data, system_architecture_icons=system_architecture_icons)
 
 @main.route('/get_skill_data/<int:index>')
 def get_skill_data(index):
     index -= 1
-    skill_data = load_json('data/main/skill.json')
+    skill_data = load_json('data/skill.json')
     if index < 1 or index > len(skill_data):
         return jsonify({"error": "Invalid index"})
     return jsonify(skill_data[index - 1])
 
 @main.route('/get_skill_nav')
 def get_skill_nav():
-    skill_data = load_json('data/main/skill.json')
+    skill_data = load_json('data/skill.json')
     return jsonify(skill_data)
