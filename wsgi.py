50b97b6cd50f2647d3fddf9253465127f1d0b1e8
unknown <waramity.re@gmail.com>
2023-09-04 20:41:46 +0700

    reverse production setting

--- wsgi.py
+++ wsgi.py
@@ -6,8 +6,8 @@ from app import app
 from app.scheduler import sched
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    # app.run(host="0.0.0.0", port=80, debug=True)
     sched.start()
 
-    # wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
+    wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
     # app.run(host="0.0.0.0", port=5001, debug=True, use_debugger=False, use_reloader=False)
